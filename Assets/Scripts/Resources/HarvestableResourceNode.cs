7f49c522fcac6c2dd8aa980fe4dbe385b5148fb8
Jason Miletta <jmiletta01@gmail.com>
2017-01-02 22:51:11 -0600

    Create base for Tree Resource node and harvesting wood

---
+++ Assets/Scripts/Resources/HarvestableResourceNode.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class HarvestableResourceNode : MonoBehaviour {
+
+    public GameObject resource;
+    public float nodeHealth;
+    public float dropCooldown = 1.0f;
+
+    private float _currentDropCooldown = 0.0f;
+
+    public void Start()
+    {
+
+    }
+
+    public void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.name == "Hammer")
+        {
+            harvestedHit(col);
+        }
+    }
+
+    private void harvestedHit(Collision col)
+    {
+        if (Time.time > _currentDropCooldown)
+        {
+            GameObject newItem = GameObject.Instantiate(resource);
+            newItem.transform.position = col.transform.position;
+            _currentDropCooldown = Time.time + dropCooldown;
+        }
+    }
+}
