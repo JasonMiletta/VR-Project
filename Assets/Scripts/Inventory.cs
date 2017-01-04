e1e96783d7fa25edd168601c9fe83f09a644e924
Jason Miletta <jmiletta01@gmail.com>
2017-01-03 23:49:14 -0600

    Start base for inventory system

---
+++ Assets/Scripts/Inventory.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Inventory : MonoBehaviour {
+
+    public List<Item> inventoryList = new List<Item>();
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void addItemToInventory(Item newItem)
+    {
+        inventoryList.Add(newItem);
+    }
+
+    public Item getItemFromInventoryByIndex(int index)
+    {
+        Item retrievedItem = inventoryList[index];
+        inventoryList.RemoveAt(index);
+
+        return retrievedItem;
+    }
+}
