bb20cdb4c73056b0a9508072f09995324155d86f
Jason Miletta <jmiletta01@gmail.com>
2017-01-25 17:28:49 -0600

    Import lowpoly water. Add ocean to scene

---
+++ Assets/LowPolyWater/Scripts/LowPolyWater.cs
@@ -0,0 +1,314 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class LowPolyWater : MonoBehaviour {
+    public Material material;
+    public Camera _camera;
+
+    void Start() {
+        if (material == null) return;
+        if(material.GetFloat("_EdgeBlend") > 0.1f) {
+            SetupCamera();
+        }
+    }
+
+    void SetupCamera() {
+        if (_camera == null) _camera = Camera.main;
+        _camera.depthTextureMode |= DepthTextureMode.Depth;
+    }
+
+    #region Editor code
+    #if UNITY_EDITOR
+    // the code below only compiles inside the editor!
+
+    public int size = 30;
+    public float scale = 1;
+    [Range(0,1)]
+    public float noise = 0;
+    public enum GridType {Hexagonal, Square};
+    public GridType gridType;
+
+    MeshFilter[] mfs;
+    bool generate;
+    float radius;
+    float prevScale;
+    float delta;
+    List<Vector3> verts = new List<Vector3>();
+    List<int> inds = new List<int>();
+
+    const int maxVerts = ushort.MaxValue;
+    const float sin60 = 0.86602540378f;
+    const float inv_tan60 = 0.57735026919f;
+
+    void OnEnable() {
+        Start();
+        mfs = GetComponentsInChildren<MeshFilter>();
+        prevScale = scale;
+        Scale();
+    }
+
+    void OnValidate() {
+        size = Mathf.Clamp(size, 1, 256);
+        mfs = GetComponentsInChildren<MeshFilter>();
+        radius = size;
+
+        generate = GUI.changed || mfs==null || mfs.Length == 0;
+        if (!generate) {
+            for (int i = 0; i < mfs.Length; i++) {
+                if(mfs[i].sharedMesh == null) {
+                    generate = true;
+                    break;
+                }
+            }
+        }
+    }
+
+    void Scale() {
+        if (material == null) return;
+        material.SetFloat("__Scale", scale);
+    }
+
+    void Update() {
+        if (material == null) return;
+
+        if (material.GetFloat("_EdgeBlend") > 0.1f) {
+            SetupCamera();
+        }
+
+        var lScale = transform.localScale;
+        if (prevScale != scale) {
+            lScale.x = scale;
+            lScale.z = scale;
+            transform.localScale = lScale;
+            prevScale = scale;
+        } else if (lScale.x != scale || lScale.z != scale) {
+            scale = Mathf.Min(lScale.x, lScale.z);
+        }
+
+        Scale();
+
+        if (!generate) return;
+        generate = false;
+        mfs = GetComponentsInChildren<MeshFilter>();
+        if (gridType == GridType.Hexagonal) {
+            GenerateHexagonal();
+        } else {
+            GenerateSquare();
+        }
+    }
+
+    float Encode(Vector3 v) {
+        var uv0 = Mathf.Round((v.x + 5) * 10000f);
+        var uv1 = Mathf.Round((v.z + 5) * 10000f) / 100000f;
+        return uv0 + uv1;
+    }
+
+    void BakeMesh(float rotation = 0f) {
+        var uvs = new List<Vector2>(inds.Count);
+        var splitIndices = new List<int>(inds.Count);
+        var splitVertices = new List<Vector3>(inds.Count);
+
+        for (int i = 0; i < inds.Count; i += 3) {
+            splitIndices.Add(i % maxVerts);
+            splitIndices.Add((i + 1) % maxVerts);
+            splitIndices.Add((i + 2) % maxVerts);
+
+            var v0 = verts[inds[i]];
+            var v1 = verts[inds[i + 1]];
+            var v2 = verts[inds[i + 2]];
+
+            splitVertices.Add(v0);
+            splitVertices.Add(v1);
+            splitVertices.Add(v2);
+
+            var uv = new Vector2();
+            uv.x = Encode(v0 - v1);
+            uv.y = Encode(v0 - v2);
+            uvs.Add(uv);
+
+            uv.x = Encode(v1 - v2);
+            uv.y = Encode(v1 - v0);
+            uvs.Add(uv);
+
+            uv.x = Encode(v2 - v0);
+            uv.y = Encode(v2 - v1);
+            uvs.Add(uv);
+        }
+
+        // clear all filters
+        for (int i = 0; i < mfs.Length; i++) {
+            DestroyImmediate(mfs[i].gameObject);
+        }
+
+        int numGO = Mathf.CeilToInt(splitVertices.Count / (float)maxVerts);
+        mfs = new MeshFilter[numGO];
+        for (int i = 0, pos = 0; i < numGO; i++, pos += maxVerts) {
+            var go = new GameObject("WaterChunk");
+            go.transform.parent = transform;
+            go.transform.localPosition = Vector3.zero;
+            go.transform.localRotation = Quaternion.Euler(0, rotation, 0);
+            go.transform.localScale = Vector3.one;
+            var mf = go.AddComponent<MeshFilter>();
+            var mr = go.AddComponent<MeshRenderer>();
+            mr.sharedMaterial = material;
+            mr.receiveShadows = false;
+            mr.shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.Off;
+
+            var mesh = new Mesh();
+            mesh.name = "WaterChunk";
+
+            var len = i == numGO - 1 ? splitVertices.Count - pos : maxVerts;
+
+            mesh.SetVertices(splitVertices.GetRange(pos, len));
+            mesh.SetTriangles(splitIndices.GetRange(pos, len), 0);
+            mesh.SetUVs(0, uvs.GetRange(pos, len));
+
+            mf.mesh = mesh;
+
+            mfs[i] = mf;
+        }
+    }
+
+    void Add(List<Vector3> verts, Vector3 toAdd) {
+        if(noise > 0) {
+            var n = UnityEngine.Random.insideUnitCircle* noise *delta / 2f;
+            toAdd.x += n.x;
+            toAdd.z += n.y;
+        }
+        verts.Add(toAdd);
+    }
+
+    void GenerateSquare() {
+        verts.Clear();
+        inds.Clear();
+        var numVerts = size*2;
+        delta = radius*2f*sin60 / numVerts;
+        var deltaX = Vector3.right * delta;
+        var vO = new Vector3(-radius *sin60, 0, -radius *sin60);
+
+        for (int j = 0; j < numVerts + 1; j++) {
+            bool reverse = j % 2 != 0;
+            var v = vO + Vector3.forward * j * delta;
+            int cols = numVerts + (reverse ? 2 : 1);
+            for (int i = 0; i < cols; i++) {
+                Add(verts, v);
+                if (reverse && (i==0||i== cols-2)) {
+                    v += deltaX / 2f;
+                } else {
+                    v += deltaX;
+                }
+            }
+        }
+        int iCur = 0;
+        for (int j = 0; j < numVerts; j++) {
+            bool reverse = j % 2 != 0;
+            int ofs = numVerts + (reverse ? 2 : 1);
+            int cols = numVerts + (reverse ? 0 : 0);
+
+            int iForw = iCur + ofs;
+
+            for (int i = 0; i < cols; i++) {
+                int iRight = iCur + 1;
+                int iForwRight = iForw + 1;
+
+                inds.Add(iCur);
+                if (reverse) {
+                    inds.Add(iForw);
+                    inds.Add(iRight);
+                    inds.Add(iForw);
+                    inds.Add(iForwRight);
+                    inds.Add(iRight);
+                } else {
+                    inds.Add(iForwRight);
+                    inds.Add(iRight);
+                    inds.Add(iCur);
+                    inds.Add(iForw);
+                    inds.Add(iForwRight);
+                }
+                iCur = iRight;
+                iForw = iForwRight;
+            }
+            inds.Add(iCur);
+            if (reverse) {
+                inds.Add(iForw );
+                inds.Add(iCur + 1);
+                iCur+=2;
+            } else {
+                inds.Add(iForw);
+                inds.Add(iForw + 1);
+                iCur++;
+            }
+        }
+
+        BakeMesh(90);
+    }
+
+    void GenerateHexagonal() {
+        verts.Clear();
+        inds.Clear();
+
+        float delta = radius / size;
+        int vertIndex = 0;
+        int curNumPoints = 0;
+        int prevNumPoints = 0;
+        int numPointsCol0 = 2 * size + 1;
+        int colMin = -size;
+        int colMax = size;
+
+        for (int i = colMin; i <= colMax; i++) {
+            float x = sin60 * delta * i;
+
+            int numPointsColi = numPointsCol0 - Mathf.Abs(i);
+
+            int rowMin = -size;
+            if (i < 0) rowMin += Mathf.Abs(i);
+
+            int rowMax = rowMin + numPointsColi - 1;
+
+            curNumPoints += numPointsColi;
+
+            for (int j = rowMin; j <= rowMax; j++) {
+                float z = inv_tan60 * x + delta * j;
+
+                var v = new Vector3(x, 0, z);
+                if (noise > 0) {
+                    var n = UnityEngine.Random.insideUnitCircle * noise * delta/2f;
+                    v.x += n.x;
+                    v.z += n.y;
+                }
+                verts.Add(v);
+
+                if (vertIndex < (curNumPoints - 1)) {
+                    if (i >= colMin && i < colMax) {
+                        int padLeft = 0;
+                        if (i < 0) padLeft = 1;
+                        inds.Add(vertIndex);
+                        inds.Add(vertIndex + 1);
+                        inds.Add(vertIndex + numPointsColi + padLeft);
+                    }
+
+                    if (i > colMin && i <= colMax) {
+                        int padRight = 0;
+                        if (i > 0)  padRight = 1;
+                        inds.Add(vertIndex+1);
+                        inds.Add(vertIndex);
+                        inds.Add(vertIndex - prevNumPoints + padRight);
+                    }
+                }
+
+                vertIndex++;
+            }
+
+            prevNumPoints = numPointsColi;
+        }
+
+        BakeMesh();
+    }
+
+
+
+    #endif
+    #endregion
+}
+
---
+++ Assets/LowPolyWater/Shaders/LowPolyWater.shader
@@ -0,0 +1,257 @@
+﻿// Upgrade NOTE: replaced '_Object2World' with 'unity_ObjectToWorld'
+
+Shader "LowPolyWater" {
+	Properties {
+		// Lighting
+		_Color ("Color", Color) = (0,0.5,0.7,0.7)
+		_Gloss ("Specular Gloss", Range(0,1)) = 0.6
+		_Specular ("Specular", Range(0.03,3)) = 0.6
+		_SpecColor("Specular Color", Color) = (1,1,1,1)
+		_Smoothness("Smoothness", Range(0,1)) = 1
+		[NoScaleOffset] _FresnelTex ("Fresnel (A) ", 2D) = "" { }
+		[KeywordEnum(Flat, VertexLit, PixelLit)] _Shading("Shading", Float) = 0
+
+		// Waves
+		[KeywordEnum(Off, LowQuality, HighQuality)]  _Waves("Enable Waves", Float) = 0
+		_Length("Wave Length", Float) = 3.3
+		_Stretch("Wave Stretch", Float) = 10
+		_Speed("Wave Speed", Float) = 0.5
+		_Height ("Wave Height", Float) = 1
+		_Steepness ("Wave Steepness", Range(0,1)) = 0.5
+		_Direction ("Wave Direction", Range(0,360)) = 180.0
+
+		//Ripples
+		_RSpeed("Ripple Speed", Float) = 1
+		_RHeight ("Ripple Height", Float) = 0.2
+
+		//Shore
+		[Toggle] _EdgeBlend("Enable Shore", Float) = 0
+		_ShoreColor("Shore Color", Color) = (1,1,1,1)
+		_ShoreIntensity("Shore Intensity", Range(-1,1)) = 0
+		_ShoreDistance("Shore Distance", Float) = 1
+
+		//Other
+		[NoScaleOffset] _NoiseTex ("Noise Texture (A)", 2D) = "white" {}
+		
+		[HideInInspector] __Direction ("__Direction", Vector) = (0,0,0,0)
+		[HideInInspector] __Scale("__Scale", Float) = 1
+		[HideInInspector] __RHeight ("__RHeight", Float) = 0.2
+		[HideInInspector] __RSpeed ("__RSpeed", Float) = 0.2
+		[HideInInspector] __TexSize("__TexSize", Float) = 64
+		[HideInInspector] __Speed("__Speed", Float) = 0
+		[HideInInspector] __Height("__Height", Float) = 0
+	}
+	SubShader {
+		Tags { "RenderType"="Transparent" "Queue"="Transparent"}
+		LOD 200
+		ZWrite Off
+		
+		Pass {
+			Name "FORWARD"
+			Tags { "LightMode" = "ForwardBase" }
+			Blend SrcAlpha OneMinusSrcAlpha
+
+			CGPROGRAM
+			#pragma target 2.0 
+			#pragma vertex vert
+			#pragma fragment frag
+
+			#pragma shader_feature _ _SHADING_VERTEXLIT _SHADING_PIXELLIT
+			#pragma shader_feature _EDGEBLEND_ON
+			#pragma shader_feature _ _WAVES_OFF _WAVES_HIGHQUALITY
+			#pragma multi_compile_fog
+
+			#include "UnityStandardUtils.cginc"
+			#include "UnityLightingCommon.cginc"
+
+			#if UNITY_VERSION < 540
+				#define UNITY_INSTANCE_ID
+				#define UNITY_VERTEX_OUTPUT_STEREO
+				#define UNITY_SETUP_INSTANCE_ID(v)
+				#define UNITY_TRANSFER_INSTANCE_ID(v,o)
+				#define UNITY_INITIALIZE_VERTEX_OUTPUT_STEREO(o)
+				#define COMPUTESCREENPOS ComputeScreenPos
+			#else
+				#define COMPUTESCREENPOS ComputeNonStereoScreenPos
+			#endif
+
+			struct appdata {
+				float4 vertex : POSITION;
+				float2 uv : TEXCOORD0;
+				UNITY_INSTANCE_ID
+			};
+
+			struct v2f {
+				float4 pos : SV_POSITION;
+				UNITY_FOG_COORDS(0)
+				#ifdef _SHADING_PIXELLIT
+					half3 worldPos : TEXOORD1;
+					half3 worldNormal : TEXOORD2;
+				#else
+					fixed3 vertexLight : TEXCOORD1;
+				#endif
+				#ifdef _EDGEBLEND_ON
+					float4 screenPos : TEXOORD3;
+				#endif
+				UNITY_INSTANCE_ID
+				UNITY_VERTEX_OUTPUT_STEREO
+			};
+
+			half __RSpeed, __RHeight, _Gloss, _Specular, _Smoothness, __TexSize, __Speed;
+			sampler2D _NoiseTex, _FresnelTex; 
+			fixed4 _Color;
+
+			#ifdef _EDGEBLEND_ON
+				sampler2D _CameraDepthTexture;
+				half _ShoreIntensity, _ShoreDistance;
+				fixed4 _ShoreColor;
+			#endif
+
+			#ifndef _WAVES_OFF
+				half __Height, _Length, _Stretch;
+				half4 __Direction; //cos, sin, cos*steepness, sin*steepness
+
+				#ifdef _WAVES_HIGHQUALITY
+					half __Scale;
+
+					inline half hash( half n ){
+					    return frac(sin(n)*43758.5453);
+					}
+
+					inline half noise( half2 x ){
+						x /= __Scale;
+					    half2 p = floor(x);
+					    half2 f = frac(x);
+					    f = smoothstep(0.0, 1.0, f); // f = f*f*(3.0-2.0*f);
+					    half n = p.x + p.y*57.0;
+					    return lerp(lerp( hash(n), hash(n+1.0),f.x), lerp( hash(n+57.0), hash(n+58.0),f.x),f.y) -0.5;
+					}
+				#else
+					inline half noise(half2 uv){
+					    return smoothstep(0,1,tex2Dlod(_NoiseTex, half4(uv/__TexSize, 0,0)).a)-0.5;
+					}
+				#endif
+
+				inline void gerstner(inout half3 p, half phase){
+					half x = p.x*__Direction.x - p.z*__Direction.y;
+					half z = p.z*__Direction.x + p.x*__Direction.y;
+					half n = noise(half2(x/_Stretch, z/_Length + phase));
+					p.y += __Height*n;
+					p.xz -= n*__Direction.wz;
+				}
+			#endif
+
+			inline half noiseTex(half2 uv){
+			    return tex2Dlod(_NoiseTex, half4(uv/__TexSize, 0,0)).a-0.5;
+			}
+
+			inline half ripple(half2 p, half phase){
+				return noiseTex(half2(p.x, phase+p.y))*__RHeight;
+			}
+
+			inline half3 lighting(half3 normal, half3 worldPos){
+				float3 lightDir = _WorldSpaceLightPos0.xyz;
+				half3 worldViewDir = normalize(UnityWorldSpaceViewDir(worldPos));
+
+				//diffuse
+				half3 diff = _Color.rgb*max(0, dot (normal, lightDir));
+
+				//ambient
+				half3 ambient = max(0, ShadeSH9(half4(normal, 1.0)));
+
+				//fresnel
+				half dn = max (0, dot( worldViewDir, normal ));
+				half3 fres = ambient * tex2Dlod(_FresnelTex, half4(dn,dn,0,0) ).a;
+				fres = lerp(diff, fres, _Smoothness);
+
+				//specular
+				half3 h = normalize (lightDir + worldViewDir);
+				half nh = max (0, dot (normal, h));
+				half3 spec = _SpecColor.rgb*pow (nh, _Specular*128.0) * _Gloss;
+
+				return _Color.rgb*ambient + _LightColor0.rgb * (fres + spec);
+			}
+
+			v2f vert (appdata v) {
+			  	UNITY_SETUP_INSTANCE_ID(v);
+				v2f o;
+				UNITY_INITIALIZE_OUTPUT(v2f,o);
+  				UNITY_TRANSFER_INSTANCE_ID(v,o);
+				UNITY_INITIALIZE_VERTEX_OUTPUT_STEREO(o);
+
+				half4 pos0 = mul(unity_ObjectToWorld, v.vertex);
+
+				// decode positions
+				half4 offs = half4(floor(v.uv),frac(v.uv)) * half4(1.0/10000.0, 1.0/10000.0, 10.0, 10.0) - 5.0;
+				float4 p = v.vertex;
+				p.xz -= offs.xz;
+				half3 pos1 = mul(unity_ObjectToWorld, p).xyz;
+				p.xz = v.vertex.xz-offs.yw;
+				half3 pos2 = mul(unity_ObjectToWorld, p).xyz;
+
+				// ripples
+				half phase = _Time[1]*__RSpeed;
+				pos0.y += ripple(pos0.xz, phase);
+				pos1.y += ripple(pos1.xz, phase);
+				pos2.y += ripple(pos2.xz, phase);
+
+				// waves
+				#ifndef _WAVES_OFF
+					phase = _Time[1]*__Speed;
+					gerstner(pos0.xyz, phase);
+					gerstner(pos1, phase);
+					gerstner(pos2, phase);
+				#endif
+
+				half3 worldNormal = cross(pos1-pos0.xyz, pos2-pos0.xyz);
+				worldNormal = normalize(worldNormal);
+
+				#ifdef _SHADING_PIXELLIT
+					o.worldNormal = worldNormal;
+					o.worldPos = pos0.xyz;
+				#elif _SHADING_VERTEXLIT
+					o.vertexLight = lighting(worldNormal, pos0.xyz);
+				#else // flat shading
+					o.vertexLight = lighting(worldNormal, (pos0.xyz+pos1+pos2)/3.0);
+				#endif
+
+				o.pos = mul(UNITY_MATRIX_VP, pos0);
+
+				#ifdef _EDGEBLEND_ON
+					o.screenPos = COMPUTESCREENPOS(o.pos);
+				#endif
+
+				UNITY_TRANSFER_FOG(o,o.pos); // pass fog coordinates to pixel shader
+				return o;
+			}
+
+
+			fixed4 frag (v2f i) : SV_Target {
+				UNITY_SETUP_INSTANCE_ID(i);
+
+				#ifdef _SHADING_PIXELLIT
+					fixed4 c = fixed4(lighting(i.worldNormal, i.worldPos), _Color.a);
+				#else
+					fixed4 c = fixed4(i.vertexLight, _Color.a);
+				#endif
+
+				#ifdef _EDGEBLEND_ON
+					half sceneZ = LinearEyeDepth(
+						tex2Dproj(_CameraDepthTexture, 
+							UNITY_PROJ_COORD(i.screenPos)).r);
+					half diff = abs(sceneZ - i.screenPos.w)/_ShoreDistance;
+					diff = smoothstep(_ShoreIntensity, 1, diff);
+                    c = lerp(_ShoreColor, c, diff);
+				#endif
+
+				UNITY_APPLY_FOG(i.fogCoord, c); // apply fog
+				return c;
+			}
+			ENDCG
+
+		} // Pass
+	} // Subshader
+
+	Fallback "VertexLit"
+	CustomEditor "LowPolyWaterShaderGUI"
+} // Shader
