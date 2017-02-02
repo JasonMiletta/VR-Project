2c0da0548f93ec249b2fa3f4766bd873512e4245
Jason Miletta <jmiletta01@gmail.com>
2017-02-01 21:48:19 -0600

    Import new World model and implement timeofdaymanager

---
+++ Assets/Scripts/TimeOfDayManager.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TimeOfDayManager : MonoBehaviour {
+
+    public float dayTime = 60f;
+
+    private float tickAngle;
+
+	// Use this for initialization
+	void Start () {
+        tickAngle = dayTime / 360f;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        this.transform.Rotate(new Vector3(0, 1), tickAngle);
+	}
+}
