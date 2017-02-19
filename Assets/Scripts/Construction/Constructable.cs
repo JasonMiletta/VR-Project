a21f41a5e8bc4363f6c96e48ab77694dc75d0320
Jason Miletta <jmiletta01@gmail.com>
2017-02-19 01:40:29 -0600

    Implement constructable objects and placements

---
+++ Assets/Scripts/Construction/Constructable.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK;
+
+public class Constructable : VRTK_InteractableObject{
+
+    public GameObject constructObject;
+
+    private ConstructionPointer constructionPointer;
+
+    void Start()
+    {
+        constructionPointer = GetComponent<ConstructionPointer>();
+    }
+
+    public override void Ungrabbed(GameObject previousGrabbingObject)
+    {
+        if (constructionPointer.IsActive())
+        {
+            if (constructObject != null)
+            {
+                GameObject constructedObject = Instantiate(constructObject, constructionPointer.pointerTip.transform.position, constructionPointer.pointerTip.transform.rotation);
+                
+            }
+        }
+        base.Ungrabbed(previousGrabbingObject);
+        
+    }
+
+    public override void StartUsing(GameObject usingObject)
+    {
+        base.StartUsing(usingObject);
+        //TODO: if pointer is on, place object
+        constructionPointer.ToggleBeam(true);
+    }
+
+    public override void StopUsing(GameObject previousUsingObject)
+    {
+        base.StopUsing(previousUsingObject);
+        constructionPointer.ToggleBeam(false);
+    }
+
+}
