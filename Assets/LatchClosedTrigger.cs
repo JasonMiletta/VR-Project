d41eea9dc5233c05ba6d8a40ebf5a43acf94c320
Jason Miletta <jmiletta01@gmail.com>
2017-06-11 01:09:44 -0500

    Addeding in Crafting box and smoke particle effects

---
+++ Assets/LatchClosedTrigger.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LatchClosedTrigger : MonoBehaviour {
+
+    public delegate void BoxClosedAction();
+    public static event BoxClosedAction OnClosed;
+
+    public ParticleSystem dustEffect;
+
+    //This is the latch we watch for to detect if the latch has been *closed*
+    public GameObject Latch;
+    public bool isClosed = true;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject == Latch)
+        {
+            Debug.Log("Box Closed!");
+            isClosed = true;
+            if(dustEffect != null)
+            {
+                dustEffect.Play();
+            }
+            OnClosed();
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.gameObject == Latch)
+        {
+            Debug.Log("Box Opened!");
+            isClosed = false;
+        }
+    }
+}
--- Assets/Scripts/ManagerScripts/CraftingItemTable.cs
+++ Assets/Scripts/ManagerScripts/CraftingItemTable.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class CraftingItemTable : MonoBehaviour {
 
+    public Item item1 = null;
+    public Item item2 = null;
+
+    public GameObject CraftingSpawnPoint;
+
     [SerializeField]
     public GameObject Sword;
     [SerializeField]
@@ -15,9 +20,32 @@ public class CraftingItemTable : MonoBehaviour {
     [SerializeField]
     public GameObject Wood;
 
+    private void OnEnable()
+    {
+        LatchClosedTrigger.OnClosed += craftItem;
+    }
+    private void OnDisable()
+    {
+        LatchClosedTrigger.OnClosed -= craftItem;
+    }
+
+    public void Start()
+    {
+
+    }
+
     public void FixedUpdate()
     {
         
+    }
+
+    public void craftItem()
+    {
+        Debug.Log("Craft Item!");
+        if (item1 != null && item2 != null)
+        {
+            craftItem(item1, item2, CraftingSpawnPoint.transform);
+        }
     }
 
     public void craftItem(Item item1, Item item2, Transform spawnPoint)
@@ -47,13 +75,53 @@ public class CraftingItemTable : MonoBehaviour {
             spawnObject(Hammer, spawnPoint);
         }
 
-        Destroy(item1);
-        Destroy(item2);
     }
 
     private void spawnObject(GameObject objectToSpawn, Transform spawnPoint)
     {
         Debug.Log("Spawning: " + objectToSpawn.name);
-        Instantiate(objectToSpawn, spawnPoint);
+        Destroy(item1.gameObject);
+        Destroy(item2.gameObject);
+
+        item1 = null;
+        item2 = null;
+
+        Instantiate(objectToSpawn, spawnPoint.position, spawnPoint.rotation);
     }
+
+    #region Triggers
+    private void OnTriggerEnter(Collider other)
+    {
+        Item collidingItem = other.gameObject.GetComponent<Item>();
+        if (collidingItem != null)
+        {
+            Debug.Log("Item entered");
+            if (item1 == null)
+            {
+                Debug.Log("item 1");
+                item1 = collidingItem;
+            }
+            else if (item2 == null)
+            {
+                Debug.Log("item 2");
+                item2 = collidingItem;
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        Item otherItem = other.gameObject.GetComponent<Item>();
+        Debug.Log("Item exited");
+        if (item1 == otherItem)
+        {
+            Debug.Log("Item 1 exited");
+            item1 = null;
+        } else if(item2 == otherItem)
+        {
+            item2 = null;
+            Debug.Log("Item 2 exited");
+        }
+    }
+    #endregion
 }
