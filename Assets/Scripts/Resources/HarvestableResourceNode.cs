0bf6c16cbda538eabfde66bbcb76010f1405d17a
Jason Miletta <jmiletta01@gmail.com>
2017-06-14 23:54:58 -0500

    Add Pinetree mesh. Add resource degradation.

--- Assets/Scripts/Resources/HarvestableResourceNode.cs
+++ Assets/Scripts/Resources/HarvestableResourceNode.cs
@@ -3,14 +3,17 @@
 public class HarvestableResourceNode : MonoBehaviour {
 
     public GameObject resource;
-    public float nodeHealth;
     public float dropCooldown = 1.0f;
 
-    private float _currentDropCooldown = 0.0f;
+    public GameObject replacementResource;
+    public Mesh[] replacementResources;
+
+    private float _currentDropCooldown = 1.0f;
+    private float currentHealth;
 
     public void Start()
     {
-
+        currentHealth = replacementResources.Length - 1;
     }
 
     public void OnCollisionEnter(Collision col)
@@ -23,11 +26,24 @@ public class HarvestableResourceNode : MonoBehaviour {
 
     private void harvestedHit(Collision col)
     {
-        if (Time.time > _currentDropCooldown)
+        if (Time.time > _currentDropCooldown && currentHealth > 0)
         {
             GameObject newItem = GameObject.Instantiate(resource);
             newItem.transform.position = col.transform.position;
             _currentDropCooldown = Time.time + dropCooldown;
+            --currentHealth;
+            swapReplacementResource();
+        }
+    }
+
+    private void swapReplacementResource()
+    {
+        GetComponent<MeshFilter>().mesh = replacementResources[(int)currentHealth];
+        GetComponent<MeshCollider>().sharedMesh = replacementResources[(int)currentHealth];
+        if (currentHealth == 0)
+        {
+            Debug.Log("0!!!!");
+            GetComponent<CapsuleCollider>().enabled = false;
         }
     }
 }
